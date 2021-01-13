72ece00311145a259a7b9e96bb0d520100d28bf6
liupeng <liupeng.0@outlook.com>
2021-01-13 10:57:53 +0800

    Function: Dairy V 2.0.7

--- functions/dairy/LiupengDairy.cpp
+++ functions/dairy/LiupengDairy.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "LiupengDairy.h"
+
+const LiupengDairyModel &LiupengDairy::getLiupengDairyModel() const {
+    return liupengDairyModel;
+}
+
+void LiupengDairy::setLiupengDairyModel(const LiupengDairyModel &liupengDairyModel) {
+    LiupengDairy::liupengDairyModel = liupengDairyModel;
+}
+
+const LiupengFile &LiupengDairy::getLiupengFile() const {
+    return liupengFile;
+}
+
+void LiupengDairy::setLiupengFile(const LiupengFile &liupengFile) {
+    LiupengDairy::liupengFile = liupengFile;
+}
--- functions/dairy/LiupengDairy.h
+++ functions/dairy/LiupengDairy.h
@@ -5,12 +5,24 @@
 #ifndef DATAPIPELINE_LIUPENGDAIRY_H
 #define DATAPIPELINE_LIUPENGDAIRY_H
 #include <iostream>
+#include "../../models/dairy/LiupengDairyModel.h"
+#include "../../models/LiupengFile.h"
 
 
 class LiupengDairy {
 
 private:
+    LiupengDairyModel liupengDairyModel;
+    LiupengFile liupengFile;
 
+public:
+    const LiupengDairyModel &getLiupengDairyModel() const;
+
+    void setLiupengDairyModel(const LiupengDairyModel &liupengDairyModel);
+
+    const LiupengFile &getLiupengFile() const;
+
+    void setLiupengFile(const LiupengFile &liupengFile);
 
 };
 
